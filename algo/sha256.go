fc774d076d419777482528dd0dacf28fd8e59b31
Nishank Priydarshi <nishankpr2002@gmail.com>
2024-10-18 21:18:33 +0530

    populated the message array

--- algo/sha256.go
+++ algo/sha256.go
@@ -17,5 +17,22 @@ func (sha256 *Sha256) Hash(str string) {
 	l := float64(len(msg)) / float64(64)
 	N := uint32(math.Ceil(l))
 
-	fmt.Println("msg is", N)
+	M := make([][]uint32, N)
+	for i := range M {
+		M[i] = make([]uint32, 16)
+	}
+
+	for i := range M {
+		for j := 0; j < 16; j++ {
+			index := i*64 + j*4
+
+			for k := 0; k < 4; k++ {
+				if index+k < len(msg) {
+					M[i][j] |= (uint32(msg[index+k]) << uint32(8*(3-k)))
+				}
+			}
+		}
+	}
+
+	fmt.Println("msg is", M)
 }
